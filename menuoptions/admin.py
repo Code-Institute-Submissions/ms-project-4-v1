73d940c3df678fa1a917c9d5b6a0e30fa5b854aa
ip69719 <ipeters@mail.com>
2022-04-06 14:51:22 +0000

    Registered MenuOption model

--- menuoptions/admin.py
+++ menuoptions/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category
+from .models import MenuOption, Category
 
 # Register your models here.
+admin.site.register(MenuOption)
 admin.site.register(Category)
