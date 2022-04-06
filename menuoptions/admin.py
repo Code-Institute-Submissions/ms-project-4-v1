827d188d6ac14a5efef075ddb1910cddf9844ed9
ip69719 <ipeters@mail.com>
2022-04-06 15:12:03 +0000

    Customised admin to add other fields

--- menuoptions/admin.py
+++ menuoptions/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from .models import MenuOption, Category
 
 # Register your models here.
+
+
+class MenuOptionAdmin(admin.ModelAdmin):
+    """ Customised admin to add fields """
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+
+ordering = ('category', 'name')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """ Customised admin to add fields """
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
 admin.site.register(MenuOption)
 admin.site.register(Category)
