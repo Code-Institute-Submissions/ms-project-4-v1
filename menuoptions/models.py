6afcbf2a68138b98e0f6edd6e2ed5c2fdefeaa5c
ip69719 <ipeters@mail.com>
2022-04-06 14:40:00 +0000

    Added docstrings to category model

--- menuoptions/models.py
+++ menuoptions/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Category(models.Model):
-
+    """ Category model """
     class Meta:
         verbose_name_plural = 'Categories'
 
@@ -12,7 +12,11 @@ class Category(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
+        """ String method """
         return self.name
 
     def get_friendly_name(self):
+        """ Model method to return friendly name """
         return self.friendly_name
+
+
