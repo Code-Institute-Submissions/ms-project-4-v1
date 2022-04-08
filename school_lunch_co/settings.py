48b0d37897a1b6bcdfb27d637c47b2fa0f2a6c7f
ip69719 <ipeters@mail.com>
2022-04-08 21:06:40 +0000

    Removed secret key and set debug

--- school_lunch_co/settings.py
+++ school_lunch_co/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b4x!i$yo0ll2omlg*d4p=l-74zk!i#d+clyozncp9922%#91^^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ip-school-lunch-co.herokuapp.com', 'localhost']
 
